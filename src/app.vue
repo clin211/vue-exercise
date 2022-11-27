22996cd061a755f660b96ac085b806cbe5e8c47a
Forest <767425412lin@gmail.com>
2022-11-27 20:08:40 +0800

    feat: buiding vue3 projects with vite

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{ count }}</h2>
+        <button @click="handleClickAdd">add</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+function handleClickAdd() {
+    count.value += 1;
+}
+</script>
+
+<style></style>
