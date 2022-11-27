04500d2f23bea1261c2353d3b5674759376e089a
Forest <767425412lin@gmail.com>
2022-11-27 19:21:11 +0800

    feat: building vue3 projects with webpack

---
+++ src/app.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="demo">
+        <div class="text">Count: {{ state.count }}</div>
+        <button class="btn" @click="onClick">Add</button>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+const state = reactive({
+    count: 0,
+});
+const onClick = () => {
+    state.count++;
+};
+</script>
+
+<style>
+.demo {
+    width: 100%;
+}
+.demo .text {
+    font-size: 28px;
+    font-weight: bolder;
+    color: #666666;
+}
+.demo .btn {
+    font-size: 20px;
+    padding: 0 10px;
+    height: 32px;
+    min-width: 80px;
+    cursor: pointer;
+}
+</style>
